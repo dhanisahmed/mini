9c792765c98188bcc0c6ba3804dc4e9c17b089af
DhanisAhmedK <dhanisahmed.k@gmail.com>
2025-05-02 21:29:06 +0530

    Update MyEnrollments.jsx

--- project/src/components/MyEnrollments.jsx
+++ project/src/components/MyEnrollments.jsx
@@ -12,7 +12,7 @@ function MyEnrollments() {
       if (!studentId) return alert("Please sign in first!");
 
       try {
-        const res = await axios.get('http://localhost:3000/courses'); // fetching all courses
+        const res = await axios.get('https://mini-9vxb.onrender.com/courses'); // fetching all courses
         const enrolled = res.data.filter(course => course.enrolledStudents.includes(studentId));
         setEnrolledCourses(enrolled);
       } catch (err) {
